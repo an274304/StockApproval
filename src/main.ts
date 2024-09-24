fdba5eda778969fc116878d24d36dda0271971f1
Gyanendra Dev <edp@committedcargo.com>
2024-09-24 10:54:44 +0530

    loging routing Error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { LogInComponent } from './app/shared/components/log-in/log-in.component';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
